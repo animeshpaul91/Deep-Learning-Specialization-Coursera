e424cf11a673fabddde5bdadcf4b85bddc2f3c2a
Animesh <ani.nitmz@gmail.com>
2019-08-10 13:40:27 -0600

    Course 5 of 5 Completed

--- Sequence Models/Week-3/td_utils.py
+++ Sequence Models/Week-3/td_utils.py
@@ -16,15 +16,18 @@ def graph_spectrogram(wav_file):
         pxx, freqs, bins, im = plt.specgram(data[:,0], nfft, fs, noverlap = noverlap)
     return pxx
 
+
 # Load a wav file
 def get_wav_info(wav_file):
     rate, data = wavfile.read(wav_file)
     return rate, data
 
+
 # Used to standardize volume of audio clip
 def match_target_amplitude(sound, target_dBFS):
     change_in_dBFS = target_dBFS - sound.dBFS
     return sound.apply_gain(change_in_dBFS)
+
 
 # Load raw audio files for speech synthesis
 def load_raw_audio():
